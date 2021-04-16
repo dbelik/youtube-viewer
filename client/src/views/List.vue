213e9e886428afe86c9a4bec91f5aa80a6022142
Denis Belik <denis.belik2014@gmail.com>
2021-04-16 16:31:00 +0300

    fetch videos from server to client side

--- client/src/views/List.vue
+++ client/src/views/List.vue
@@ -1,14 +1,25 @@
 <template>
     <div>
         <h1>Test</h1>
+        <ul>
+            <li v-for="(video, id) in videos" v-bind:key="id">
+                <iframe width="420" height="315" :src="`https://www.youtube.com/embed/${video}`"></iframe>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
 import axios from 'axios'
 
 export default {
-    created() {
-        axios.get('localhost:8080');
+    data() {
+        return {
+            videos: []
+        };
+    },
+    async created() {
+        const data = await axios.get('http://localhost:8081/list.php');
+        this.videos = data.data;
     }
 }
 </script>
